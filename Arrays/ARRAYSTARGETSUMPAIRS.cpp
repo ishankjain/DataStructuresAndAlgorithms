a45550830f1fb983f2dc1bd5e19a9bbb50126648
iamarchit2 <architaggarwal0508@gmail.com>
2019-03-09 20:05:26 +0530

    Arrays Modified

---
+++ Arrays/ARRAYSTARGETSUMPAIRS.cpp
@@ -0,0 +1,23 @@
+//Deepak Aggarwal, Coding Blocks
+//deepak@codingblocks.com
+
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    int array[N];
+    for (int i = 0; i < N; i++) {
+        cin >> array[i];
+    }
+    int target;
+    cin >> target;
+    for (int i = 0; i < N; i++) {
+        int reqnum = target - array[i];
+        for (int j = i + 1; j < N; j++) {
+            if (array[j] == reqnum) {
+                cout << min(array[i], array[j]) << " and " << max(array[j], array[i]) << endl;
+            }
+        }
+    }
+}
