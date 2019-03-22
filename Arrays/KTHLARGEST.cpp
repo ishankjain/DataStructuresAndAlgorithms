53673e44a3bee1fe8e441e190960a5d00bde5c92
iamarchit2 <architaggarwal0508@gmail.com>
2019-03-22 11:37:37 +0530

    Modified

---
+++ Arrays/KTHLARGEST.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    int array[N];
+    for (int i = 0; i < N; i++) {
+        cin >> array[i];
+    }
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (array[j] > array[i]) {
+                int temp = array[j];
+                array[j] = array[i];
+                for (int k = i - 1; k > j; k--) {
+                    array[k + 1] = array[k];
+                }
+                array[j + 1] = temp;
+                break;
+            }
+        }
+    }
+    int M;
+    cin >> M;
+    cout << array[N - M];
+}
