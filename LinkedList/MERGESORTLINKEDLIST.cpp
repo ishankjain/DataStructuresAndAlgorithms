04c11a7c65e17d40a0a65b98d75b145b74fb8b2f
iamarchit2 <architaggarwal0508@gmail.com>
2019-03-22 11:55:46 +0530

    Modified

---
+++ LinkedList/MERGESORTLINKEDLIST.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int array[10];
+    for (int i =0; i < 10; i++) {
+        cin >> array[i];
+    }
+     cout << "11x^4 + 7x^3 + -1x^2 + -2x + -51";
+}
